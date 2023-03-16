968f484a86075eefd71e8adf0fd2accc43f8410e
omarelzhery <omarelzhery@gmail.com>
2023-03-16 13:56:35 -0700

    sfsdf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *main- A program that uses puts to print a string
+ *Return: ends with 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
