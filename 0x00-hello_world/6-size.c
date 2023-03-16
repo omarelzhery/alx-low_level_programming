ee8b626bea3198707bdcc24d638555317fca2e19
omarelzhery <omarelzhery@gmail.com>
2023-03-16 15:32:57 -0700

    sfsdf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main- A program that uses puts to print a string
+ *Return: ends with 0
+ */
+int main(void)
+{
+printf("Size of char: %ld byte(s)\n" sizeof(char));
+printf("Size of int: %ld byte(s)\n" sizeof(int));
+printf("Size of float: %ld byte(s)\n" sizeof(float));
+printf("Size of long int: %ld byte(s)\n" sizeof(long int));
+printf("Size of long long int: %ld byte(s)\n" sizeof(long long int));
+return (0);
+}
