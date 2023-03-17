9d46c6f9739f231a984f1bada21994cfb19d3ff2
omarelzhery <omarelzhery@gmail.com>
2023-03-17 11:30:39 -0700

    newupdate

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = "a"; x <= "z"; x++)
+	{
+	if (x == "e" || x == "q")
+	{
+	continue;
+	}
+	putchar(x);
+	putchar("\n");
+	return (0);
+	}
+}
