8a0dad7e4d0dfbbe37047d430304a2c51aaaa843
omarelzhery <omarelzhery@gmail.com>
2023-03-17 12:24:00 -0700

    newupdate

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Main dfsd
+ * sfsd
+ * sdf
+ * return
+ * int x sdfd
+*/
+
+main int(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+
+	putchar("%d", x);
+	putchar("\n");
+	return (0);
+}
