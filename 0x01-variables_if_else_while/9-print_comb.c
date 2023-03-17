aaa2051a6c0700abee2f46b07abd7e452442c81e
omarelzhery <omarelzhery@gmail.com>
2023-03-17 13:35:32 -0700

    newupdate

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Prints all the numbers in base 16;
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 0; x < 10; x++)
+	{
+	putchar(x);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
