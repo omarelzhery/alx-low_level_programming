332bb62354cf7ae1ab5c6c50f1bfe22f50f03ced
omarelzhery <omarelzhery@gmail.com>
2023-03-21 15:23:13 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point ,this prints alphabet
+ * in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+
+	for (int s = 0; s < 10; s++)
+	{
+		for (alpha = "a"; a <= "z"; alpha++)
+		{
+			_putchar(alpha);
+			_putchar("/n");
+		}
+	}
+}
