792f851af27713b2196b934277f13f15e1831a06
omarelzhery <omarelzhery@gmail.com>
2023-03-21 16:06:49 -0700

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if input alphabet(in this example
+ * letter c).
+ * @c: the letter being checked
+ *
+ * Return: 1 if lowercase, else 0.
+ */
+
+int _islower(int c)
+{
+	if (c.isLower == true)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@ int _putchar(char b);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int print_sign(int n);
+int _islower(int c);
