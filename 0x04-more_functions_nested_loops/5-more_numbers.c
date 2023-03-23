ce87874a607a32faa7fd15779c1e9db1e8c0a3bf
omarelzhery <omarelzhery@gmail.com>
2023-03-23 16:26:17 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,20 @@
 
 void more_numbers(void)
 {
-	char c;
-	int i;
+	int num, row, count;
 
-	for (i = 1; i <= 10; i++)
+	for (row = 1; row <= 10; row++)
 	{
-		for (c = '0'; c <= '14'; c++)
+		for (count = 0; count <= 14; count)
 		{
-			_putchar(c);
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
