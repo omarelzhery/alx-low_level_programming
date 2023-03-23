2243ad31cb13f2e15fcb2e3a3083e8fef5eaf7f0
omarelzhery <omarelzhery@gmail.com>
2023-03-23 14:49:30 -0700

    new

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0123456789
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char b;
+	for ( b = '0'; b < '10'; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+}
