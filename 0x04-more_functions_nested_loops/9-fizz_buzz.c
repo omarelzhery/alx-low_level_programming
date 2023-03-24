ae4b7ecbae791e7e639f6d0a6023aed43109aa6d
omarelzhery <omarelzhery@gmail.com>
2023-03-23 19:13:36 -0700

    new

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,13 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && !(num % 5 == 0))
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0 && !(num % 3 == 0))
-		{
 			printf("Buzz");
-		}
 		else if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else
-		{
 			printf("%d", i);
-		}
 		if (i != 100)
 			printf(" ");
 		else
