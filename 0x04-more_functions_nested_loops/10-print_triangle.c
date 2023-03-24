f09eff447520c8316ceaa7a71744328ff40b268e
omarelzhery <omarelzhery@gmail.com>
2023-03-23 19:29:51 -0700

    new

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -20,7 +20,7 @@ void print_triangle(int size)
 		{
 			for (base = 1; base <= size; base++)
 			{
-				if ((high + base) <= size)
+				if ((hight + base) <= size)
 					_putchar(' ');
 				else
 					_putchar('#');
