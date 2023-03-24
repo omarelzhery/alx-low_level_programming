714d7ad8ba3e49940952bd0933427f545bd64053
omarelzhery <omarelzhery@gmail.com>
2023-03-23 17:10:00 -0700

    new

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ * @n 
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int ln;
+
+	if (n == 0)
+		_putchar('\n');
+	else
+	{
+		for (ln = 1; ln <= n; ln++)
+		{	
+			for (space = 1; space <= ln; space++)
+				_putchar(' ');
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+}
