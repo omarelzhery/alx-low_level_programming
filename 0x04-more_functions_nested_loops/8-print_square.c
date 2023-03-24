936a9accce073cf9cfa63e2cd685b43412968b79
omarelzhery <omarelzhery@gmail.com>
2023-03-23 18:30:44 -0700

    new

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,6 +11,7 @@ void print_square(int size)
 {
 
 	int row, column;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -20,7 +21,9 @@ void print_square(int size)
 		for (row = 1; row <= size; row++)
 		{
 			for (column = 1; colum <= size; column++)
+			{
 				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
