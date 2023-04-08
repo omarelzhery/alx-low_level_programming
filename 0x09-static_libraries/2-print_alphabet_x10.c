e65577d309305007407a976f3c71482b714f0486
omarelzhery <119765541+omarelzhery@users.noreply.github.com>
2023-04-08 21:37:51 +0200

    Add files via upload

---
+++ 0x09-static_libraries/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point ,this prints alphabet
+ * in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int s = 1;
+	char alpha;
+
+
+	while (s++ <= 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+
+
+		_putchar('\n');
+	}
+}
