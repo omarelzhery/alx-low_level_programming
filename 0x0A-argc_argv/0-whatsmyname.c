4e4a97ee04bbcf6764eed5743da33c32c1b824a3
omarelzhery <omarelzhery@gmail.com>
2023-04-09 12:43:27 -0700

    sdf

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/** main : takesd
+ *
+ * inr argc :0
+ * return : 0
+ **/
 int main(int argc, char *argv[])
 {
-	printf("%s", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
