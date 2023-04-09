a51d0a34636cc194268044b90bbeeed253377446
omarelzhery <omarelzhery@gmail.com>
2023-04-09 13:30:01 -0700

    sdf

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - takesd
+ *
+ * Description: 'the program's description'
+ *
+ * @argc : 0sdfs
+ * @argv : 4dsfsd
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argv[i]);
+	}
+	return (0);
+}
