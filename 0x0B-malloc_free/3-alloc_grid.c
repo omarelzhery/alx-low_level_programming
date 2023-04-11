99eea41405aec18d4fd982748617efd0799b304c
omarelzhery <omarelzhery@gmail.com>
2023-04-11 16:52:24 -0700

    sdfsdf

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+ /**
+ * alloc_grid - create array of siz sdfv 3
+ * @width: sdfsklj
+ * @height: skjdfn
+ * Description: create array of size
+ * Return: pointer
+ *
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+	return (grid);
+}
