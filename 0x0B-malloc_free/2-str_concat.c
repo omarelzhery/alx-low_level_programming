50ba374fb48ae8dedeb79cb8004f316234929a74
omarelzhery <omarelzhery@gmail.com>
2023-04-12 17:41:57 -0700

    sdfsdf

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,7 +27,8 @@ char *str_concat(char *s1, char *s2)
 
 	s1_len = strlen(s1);
 	s2_len = strlen(s2);
-	*result = malloc(s1_len + s2_len + 1);
+	
+	result = malloc(s1_len + s2_len + 1);
 
 	if (result == NULL)
 	{
