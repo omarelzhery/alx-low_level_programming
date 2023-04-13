12189c1c1614a3b4e5a8129c79135daaf44734e6
omarelzhery <omarelzhery@gmail.com>
2023-04-13 16:12:11 -0700

    sdfds

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - create array of size
+ *
+ * @ptr: aize of arry
+ * char to assign
+ * Description: create array of size
+ * Return: pointer
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+    if (!ptr)
+    {
+	    exit(98);
+    }
+    return (ptr);
+}
