c528bf6b95a90eb3dbd71facafdf22131ee4d0c4
omarelzhery <omarelzhery@gmail.com>
2023-04-17 00:18:44 -0700

    SDFd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog's properties
+ * @name: pointer to a char array for the dog's name
+ * @age: the age of the dog
+ * @owner: pointer to a char array for the owner's name
+ *
+ * Description: defines a dog's properties using a struct
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
