d9ea2ee0a02b0db49430ec3f3ee49777aaf92275
omarelzhery <omarelzhery@gmail.com>
2023-04-17 00:36:56 -0700

    SDFd

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - sdfsfd
+ * @name :sdfs
+ * @age :sdf
+ * @owner :Sdf
+ * @d :sdf
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
