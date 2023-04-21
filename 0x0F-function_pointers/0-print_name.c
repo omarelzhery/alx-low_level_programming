fe1ac4dfd9d55c0a3c6d82343868e5123c41feae
omarelzhery <omarelzhery@gmail.com>
2023-04-21 16:16:55 -0700

    Sdf

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: a pointer to a character array representing the name to be printed
+ * @f: a pointer to a function that takes a character array and prints it
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	printf("Name: ");
+	(*f)(name);
+}
+
+/**
+ * print_string - prints a string
+ * @str: a pointer to a character array representing the string to be printed
+ */
+void print_string(char *str)
+{
+	printf("%s", str);
+}
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char name[] = "John Smith";
+	print_name(name, print_string);
+	return (0);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+void print_name(char *name, void (*f)(char *));
+#endif
